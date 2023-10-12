9db2f2fc56ec5512084b026140f897d61a552683
BlenTesfaye <blent3124@gmail.com>
2023-10-12 09:51:10 -0400

    lowercase and arrays

--- seed/flowers.js
+++ seed/flowers.js
@@ -33,16 +33,16 @@ async function seedFlowers() {
     console.log(typeof (Flower));
 
     const flowerSeedData = [
-      { "name": "Rose", "color": "Red", "price": 2.99 },
-      { "name": "Tulip", "color": "Yellow", "price": 1.99 },
-      { "name": "Daisy", "color": "White", "price": 1.49 },
-      { "name": "Lily", "color": "Pink", "price": 3.49 },
-      { "name": "Sunflower", "color": "Yellow", "price": 2.79 },
-      { "name": "Orchid", "color": "Purple", "price": 4.99 },
-      { "name": "Carnation", "color": "Pink", "price": 1.89 },
-      { "name": "Daffodil", "color": "Yellow", "price": 2.29 },
-      { "name": "Chrysanthemum", "color": "Orange", "price": 3.19 },
-      { "name": "Hydrangea", "color": "Blue", "price": 4.29 }
+      { "name": "rose", "color": "red", "price": 2.99 },
+      { "name": "tulip", "color": "yellow", "price": 1.99 },
+      { "name": "daisy", "color": "white", "price": 1.49 },
+      { "name": "lily", "color": "pink", "price": 3.49 },
+      { "name": "sunflower", "color": "yellow", "price": 2.79 },
+      { "name": "orchid", "color": "purple", "price": 4.99 },
+      { "name": "carnation", "color": "pink", "price": 1.89 },
+      { "name": "daffodil", "color": "yellow", "price": 2.29 },
+      { "name": "chrysanthemum", "color": "orange", "price": 3.19 },
+      { "name": "hydrangea", "color": "blue", "price": 4.29 }
     ];
 
     // Clear existing data in the flowers collection
